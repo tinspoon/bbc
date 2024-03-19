a16896c9c3badf8186a7f4be91503a56364d4d6b
tinspoon <tinspoon42@gmail.com>
2024-03-18 23:44:17 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+import logo from './bbc.png';
+import twitterLogo from './twitterLogo.png';
+import telegramLogo from './telegramLogo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +9,38 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BBC on Solana
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://birdeye.so/token/?chain=solana"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Trade BBC (Coming Soon!)
         </a>
+        <div>
+        <span>
+        <a
+          className="App-link"
+          href="https://x.com/BIGBLACKCOCKSOL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <img src={twitterLogo} width={100} height={100} alt="logo" />
+        </a>
+        </span>
+        <span>
+        <a
+          className="App-link"
+          href="https://t.me/BigBlackCockOnSolana"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <img src={telegramLogo} width={100} height={100} style={{marginLeft:"1rem"}} alt="logo" />
+        </a>
+        </span>
+        </div>
       </header>
     </div>
   );
